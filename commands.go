6501cbd2a47d42af19db30c4aff2b2668d99d218
Bruno Fidelis <bruno.fidelis004@gmail.com>
2015-09-24 21:53:27 -0300

    Initial commit

---
+++ commands.go
@@ -0,0 +1,17 @@
+package gync
+
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+)
+
+func RegisterCommands() []cli.Command {
+	return []cli.Command {
+		Name: 		"add",
+		Aliases: 	nil,
+		Usage: 		"Add a new directory/file to be watched",
+		Action: func(c *cli.Context) {
+			fmt.Println("diretorio adicionado: ", c.Args().First())
+		},
+	}
+}
---
+++ gync.go
@@ -0,0 +1,82 @@
+package gync 
+
+import (
+	"os"
+	"fmt"
+	"log"
+	"encoding/json"
+	"github.com/stacktic/dropbox"
+	"github.com/codegangsta/cli"
+)
+
+var DropboxInfo struct {
+	ClientId string
+	ClientSecret string
+	Token string
+}
+
+
+func listenOn(file string) {
+	info, err := os.Stat(file)
+
+	if err == nil {
+		lastChangedOn := info.ModTime()
+
+		for {
+		
+			info, err := os.Stat(file)
+			
+			if err == nil {
+
+				if info.ModTime().After(lastChangedOn) {
+					fmt.Println("arquivo alterado")
+					lastChangedOn = info.ModTime()
+				}
+			}
+		}
+	}
+
+}
+
+func main() {
+
+	file, err := os.Open("storage/dropbox.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonInfo := json.NewDecoder(file)
+
+	if err = jsonInfo.Decode(&DropboxInfo); err != nil {
+		log.Fatal(err)
+	}
+
+	dropBox := dropbox.NewDropbox()
+	dropBox.SetAppInfo(DropboxInfo.ClientId, DropboxInfo.ClientSecret)
+	dropBox.SetAccessToken(DropboxInfo.Token)
+
+	app := cli.NewApp()
+	app.Name = "Gync"
+	app.Usage = "Keep files and directories synced with in real time"
+
+	app.Flags = []cli.Flag {
+		cli.StringFlag{
+			Name: "lang",
+			Value: "peido",
+			Usage: "peido na cara",
+		},
+	}
+
+	app.Commands = gync.RegisterCommands();
+
+	app.Run(os.Args)
+
+
+  /*if _, err := dropBox.CreateFolder("UnoMas"); err != nil {
+		fmt.Printf("Error creating folder %s \n", err)
+	} else {
+		fmt.Println("Folder created !")
+	}*/
+}
+
