11f371d0a812b19da7a94fa43427d69d14b2bb65
Bruno Fidelis <bruno.fidelis004@gmail.com>
2015-09-30 22:17:44 -0300

    thats gonna take some time =(

--- core/whatcher.go
+++ core/whatcher.go
@@ -9,7 +9,8 @@ import (
 )
 
 type Watcher struct {
-	ModTimes []time.Time
+	ModTimes 	[]time.Time
+	Name		string
 }
 
 /**
@@ -43,47 +44,56 @@ func (w *Watcher) WalkPopulate(path string, info os.FileInfo, err error) error {
 		return err
 	}
 
+	w.ModTimes = make([]time.Time, 0)
 	w.ModTimes = append(w.ModTimes, info.ModTime())
 
 	return err
 }
 
-func (m *mirror) WalkMirror(path string, info os.FileInfo, err error) error {
-	if err != nil {
-		log.Fatal(err)
-		return err
-	}
-
-	fmt.Println(w.ModTimes[*w.Step])
-
-	return err	
-}
-
 func (w *Watcher) ObserveDir(path string) {
 	err := filepath.Walk(path, w.WalkPopulate)
 	mirror := new(Watcher) 
-
+	
 	if err != nil {
 		log.Fatal("ON DAEMON: ", err)
+		return
 	}
 
-	copy(mirror.ModTimes, w.ModTimes)
+	mirror.ModTimes = make([]time.Time, len(w.ModTimes))
 
+	copy(mirror.ModTimes, w.ModTimes)
+	
 	for {
 		err := filepath.Walk(path, mirror.WalkPopulate)
 		
 		if err != nil {
 			log.Fatal("ON DAEMON: ", err)
+			return
 		}
 
-		if w.check(mirror) {
-			copy(w.ModTimes, mirror.ModTimes)
+		if w.check(mirror.ModTimes) {
+			fmt.Println("alterado")
+			copy(w.ModTimes, mirror.ModTimes)		
 		}
+
+		mirror.ModTimes = make([]time.Time, len(w.ModTimes), len(w.ModTimes))
 	}
 }
 
-func (w *Watcher) check() bool {
+func (w *Watcher) check(mirror []time.Time) bool {
+	if len(w.ModTimes) != len(mirror) {
+		fmt.Println(len(w.ModTimes)," != ", len(mirror))
+		return true
+	}
 
+	for i := 0; i < len(mirror); i++ {
+		if ! w.ModTimes[i].Equal(mirror[i]) {
+			fmt.Println(w.ModTimes[i]," != ", mirror[i])
+			return true
+		}
+	}
+
+	return false
 }
 
 
--- core/whatcher_test.go
+++ core/whatcher_test.go
@@ -8,9 +8,7 @@ import (
 func TestObserveFile(t *testing.T) {
 	//ObserveFile("../t")
 	var w Watcher
-
-	*w.Step = 0
-
+	
 	w.ObserveDir("../t")
 	
 	fmt.Println("continuing with the program")
